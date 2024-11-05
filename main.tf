fd13d57a405547bc7f0ee643c6f3ab4143d741f8
dev98reynaga <108247670+dev98reynaga@users.noreply.github.com>
2024-11-05 08:37:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mi_servidor" {
-  ami           = "ami-xxxxxxxx" # Cambia a la AMI que necesitas
+  ami           = "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
   tags = {
     Name = "${var.salon}-instance"
