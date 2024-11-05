b5f085f031911d5b50188c4500684d0ad536f32b
dev98reynaga <108247670+dev98reynaga@users.noreply.github.com>
2024-11-05 09:12:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,22 @@ resource "aws_instance" "mi_servidor" {
     Name = "${var.salon}-instance"
   }
 }
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-static-site-bucket"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.static_site.bucket
+  key    = "index.html"
+  source = "index.html"  # Asegúrate de tener este archivo en el mismo directorio
+  acl    = "public-read"
+}
+
+output "site_url" {
+  value = aws_s3_bucket.static_site.website_endpoint
+}
