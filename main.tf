69b6212873977da7e0e8e1874b59fced8e530eef
dev98reynaga <108247670+dev98reynaga@users.noreply.github.com>
2024-11-05 09:15:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-east-1"  # Cambia a tu región preferida
+}
+
+# Configuración de la instancia EC2
 resource "aws_instance" "mi_servidor" {
   ami           = "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
@@ -6,14 +11,18 @@ resource "aws_instance" "mi_servidor" {
   }
 }
 
+# Configuración del bucket S3 para sitio web estático
 resource "aws_s3_bucket" "static_site" {
-  bucket = "my-static-site-bucket"
+  bucket = "my-unique-static-site-bucket-12345"  # Cambia a un nombre único
 
   website {
     index_document = "index.html"
   }
+
+  acl = "public-read"
 }
 
+# Cargar el archivo index.html en el bucket S3
 resource "aws_s3_bucket_object" "index" {
   bucket = aws_s3_bucket.static_site.bucket
   key    = "index.html"
@@ -21,6 +30,7 @@ resource "aws_s3_bucket_object" "index" {
   acl    = "public-read"
 }
 
+# Output para la URL del sitio web estático en S3
 output "site_url" {
   value = aws_s3_bucket.static_site.website_endpoint
 }
